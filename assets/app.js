db54715fec2228dae8fe05e90b4eea0c33de9afa
padawan95 <zeli.armel@gmail.com>
2021-09-17 21:19:52 +0200

    Adding webpack encore and comment vote (v1) feature

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+// start the Stimulus application
+import './bootstrap';
+
+let container = document.querySelectorAll('.js-vote-arrows')
+
+container.forEach(function (elementDiv) {
+    elementDiv.querySelectorAll('a').forEach(function (elementLink) {
+        elementLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            fetch(
+                "/comments/10/vote/" + elementLink.dataset.direction, {method: 'POST'}
+            ).then((response) => response.json())
+                .then((data) => {elementDiv.querySelector('.js-vote-total').textContent = data.votes})
+                .catch(error => console.log('Un problème est survenu ' + error.message))
+        })
+    })
+})
